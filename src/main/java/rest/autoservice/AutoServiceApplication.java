a19fcc5ee1daa261148f34ce1fbe230fc751dfa1
X3NON-135 <immort4ld3momo@gmail.com>
2022-11-29 23:46:01 +0000

    changed naming

--- src/main/java/rest/autoservice/AutoServiceApplication.java
+++ src/main/java/rest/autoservice/AutoServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AutoserviceApplication {
+public class AutoServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AutoserviceApplication.class, args);
+        SpringApplication.run(AutoServiceApplication.class, args);
     }
 
 }
