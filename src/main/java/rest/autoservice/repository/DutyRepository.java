4c29a866d2014175ff22e9a045412eb145a426ee
X3NON-135 <immort4ld3momo@gmail.com>
2022-12-01 21:44:17 +0000

    created repository

--- src/main/java/rest/autoservice/repository/DutyRepository.java
+++ src/main/java/rest/autoservice/repository/DutyRepository.java
@@ -1,9 +1,17 @@
 package rest.autoservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import rest.autoservice.model.Duty;
 
+import javax.transaction.Transactional;
+
 @Repository
 public interface DutyRepository extends JpaRepository<Duty, Long> {
+    @Transactional
+    @Modifying
+    @Query("UPDATE Duty d SET d.paymentStatus = :newStatus WHERE d.id = :id")
+    void updateDutyStatusById(Long id, Duty.PaymentStatus newStatus);
 }
