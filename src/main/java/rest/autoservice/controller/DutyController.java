1112111be1b867c1a15571148166e72e70b5f536
X3NON-135 <immort4ld3momo@gmail.com>
2022-12-01 21:48:55 +0000

    created controllers

--- src/main/java/rest/autoservice/controller/DutyController.java
+++ src/main/java/rest/autoservice/controller/DutyController.java
@@ -1,10 +1,12 @@
 package rest.autoservice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import rest.autoservice.dto.duties.DutyRequestDto;
 import rest.autoservice.dto.duties.DutyResponseDto;
@@ -12,18 +14,22 @@ import rest.autoservice.mapper.RequestDtoMapper;
 import rest.autoservice.mapper.ResponseDtoMapper;
 import rest.autoservice.model.Duty;
 import rest.autoservice.service.DutyService;
+import rest.autoservice.service.DutyStatusService;
 
 @RestController
 @RequestMapping("/duties")
 public class DutyController {
     private final DutyService dutyService;
+    private final DutyStatusService dutyStatusService;
     private final RequestDtoMapper<DutyRequestDto, Duty> requestMapper;
     private final ResponseDtoMapper<DutyResponseDto, Duty> responseMapper;
 
     public DutyController(DutyService dutyService,
+                          DutyStatusService dutyStatusService,
                           RequestDtoMapper<DutyRequestDto, Duty> requestMapper,
                           ResponseDtoMapper<DutyResponseDto, Duty> responseMapper) {
         this.dutyService = dutyService;
+        this.dutyStatusService = dutyStatusService;
         this.requestMapper = requestMapper;
         this.responseMapper = responseMapper;
     }
@@ -42,12 +48,11 @@ public class DutyController {
         return responseMapper.toDto(duty);
     }
 
-    @PutMapping("/{id}/update-status")
+    @GetMapping("/{id}/update-status")
     public DutyResponseDto updateStatus(@PathVariable Long id,
-                                        @RequestBody String status) {
-        Duty duty = dutyService.findById(id);
-        duty.setId(id);
-        duty.setPaymentStatus(Duty.PaymentStatus.valueOf(status));
-        return responseMapper.toDto(dutyService.save(duty));
+                                        @RequestParam("status") String paymentStatus) {
+        Duty updatedDuty = dutyService.save(
+                dutyStatusService.updateDutyStatusById(id, Duty.PaymentStatus.valueOf(paymentStatus.toUpperCase())));
+        return responseMapper.toDto(updatedDuty);
     }
 }
