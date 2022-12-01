8ae4a76f9f074b678e1433192f99f0e76c7fefa6
X3NON-135 <immort4ld3momo@gmail.com>
2022-12-01 23:49:34 +0000

    created service

--- src/main/java/rest/autoservice/service/DutyService.java
+++ src/main/java/rest/autoservice/service/DutyService.java
@@ -2,8 +2,12 @@ package rest.autoservice.service;
 
 import rest.autoservice.model.Duty;
 
+import java.util.List;
+
 public interface DutyService {
     Duty save(Duty duty);
 
     Duty findById(Long id);
+
+    List<Duty> getDutyByMasterIdAndOrOrderId(Long masterId, Long orderId);
 }
