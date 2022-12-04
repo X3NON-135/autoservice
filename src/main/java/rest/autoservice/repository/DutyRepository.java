cd839cdecae297b4077ce4fb87f635bb3ceaee47
X3NON-135 <immort4ld3momo@gmail.com>
2022-12-04 16:54:07 +0000

    created repository

--- src/main/java/rest/autoservice/repository/DutyRepository.java
+++ src/main/java/rest/autoservice/repository/DutyRepository.java
@@ -1,12 +1,12 @@
 package rest.autoservice.repository;
 
+import java.util.List;
+import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import rest.autoservice.model.Duty;
-
-import javax.transaction.Transactional;
 
 @Repository
 public interface DutyRepository extends JpaRepository<Duty, Long> {
@@ -14,4 +14,6 @@ public interface DutyRepository extends JpaRepository<Duty, Long> {
     @Modifying
     @Query("UPDATE Duty d SET d.paymentStatus = :newStatus WHERE d.id = :id")
     void updateDutyStatusById(Long id, Duty.PaymentStatus newStatus);
+
+    List<Duty> getDutyByMasterIdAndOrOrderId(Long masterId, Long orderId);
 }
--- src/main/java/rest/autoservice/repository/MasterRepository.java
+++ src/main/java/rest/autoservice/repository/MasterRepository.java
@@ -1,9 +1,24 @@
 package rest.autoservice.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import rest.autoservice.model.Master;
+import rest.autoservice.model.Order;
 
 @Repository
 public interface MasterRepository extends JpaRepository<Master, Long> {
+    @Query("SELECT o FROM Order o "
+            + " INNER JOIN FETCH o.duties d "
+            + " INNER JOIN FETCH d.master m "
+            + " WHERE m.id = :id")
+    List<Order> getCompletedOrdersById(Long id);
+
+    @Query("SELECT m FROM Master m WHERE m.id in "
+            + " (SELECT o FROM Order o "
+            + " INNER JOIN FETCH o.duties d "
+            + " INNER JOIN FETCH d.master dMas"
+            + " WHERE o.id = :id)")
+    List<Master> getAllMastersByOrderId(Long id);
 }
