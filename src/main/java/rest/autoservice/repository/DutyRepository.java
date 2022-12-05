02a7a5904ee31f6fd62870fcb0ee0a581e006bd9
X3NON-135 <immort4ld3momo@gmail.com>
2022-12-05 19:14:11 +0000

    created repository

--- src/main/java/rest/autoservice/repository/DutyRepository.java
+++ src/main/java/rest/autoservice/repository/DutyRepository.java
@@ -1,6 +1,5 @@
 package rest.autoservice.repository;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import rest.autoservice.model.Duty;
