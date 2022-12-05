97fe4449a77b7daa8fbb4d50c49bb24f4b488997
X3NON-135 <immort4ld3momo@gmail.com>
2022-12-05 17:51:43 +0000

    created repository

--- src/main/java/rest/autoservice/repository/AutoOwnerRepository.java
+++ src/main/java/rest/autoservice/repository/AutoOwnerRepository.java
@@ -1,9 +1,12 @@
 package rest.autoservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import rest.autoservice.model.AutoOwner;
 
 @Repository
 public interface AutoOwnerRepository extends JpaRepository<AutoOwner, Long> {
+    @Query("SELECT ao FROM AutoOwner ao INNER JOIN ao.autos a where a.id = :id")
+    AutoOwner getAutoOwnerByAutoId(Long id);
 }
--- src/main/java/rest/autoservice/repository/DutyRepository.java
+++ src/main/java/rest/autoservice/repository/DutyRepository.java
@@ -1,19 +1,10 @@
 package rest.autoservice.repository;
 
 import java.util.List;
-import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import rest.autoservice.model.Duty;
 
 @Repository
 public interface DutyRepository extends JpaRepository<Duty, Long> {
-    @Transactional
-    @Modifying
-    @Query("UPDATE Duty d SET d.paymentStatus = :newStatus WHERE d.id = :id")
-    void updateDutyStatusById(Long id, Duty.PaymentStatus newStatus);
-
-    List<Duty> getDutyByMasterIdAndOrOrderId(Long masterId, Long orderId);
 }
