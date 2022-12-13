01c38a275baa9e8ed006ea6240e2d900dde596e0
X3NON-135 <immort4ld3momo@gmail.com>
2022-12-13 10:38:33 +0000

    fixed checkstyle

--- src/main/java/rest/autoservice/controller/OrderController.java
+++ src/main/java/rest/autoservice/controller/OrderController.java
@@ -1,7 +1,6 @@
 package rest.autoservice.controller;
 
 import io.swagger.annotations.ApiOperation;
-import java.time.LocalDateTime;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,32 +15,23 @@ import rest.autoservice.dto.product.ProductRequestDto;
 import rest.autoservice.mapper.RequestDtoMapper;
 import rest.autoservice.mapper.ResponseDtoMapper;
 import rest.autoservice.mapper.impl.ProductMapper;
-import rest.autoservice.model.AutoOwner;
 import rest.autoservice.model.Order;
-import rest.autoservice.service.AutoOwnerService;
 import rest.autoservice.service.OrderService;
-import rest.autoservice.service.ProductService;
 
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
     private final OrderService orderService;
     private final ProductMapper productMapper;
-    private final ProductService productService;
-    private final AutoOwnerService autoOwnerService;
     private final RequestDtoMapper<OrderRequestDto, Order> requestMapper;
     private final ResponseDtoMapper<OrderResponseDto, Order> responseMapper;
 
     public OrderController(OrderService orderService,
                            ProductMapper productMapper,
-                           ProductService productService,
-                           AutoOwnerService autoOwnerService,
                            RequestDtoMapper<OrderRequestDto, Order> requestMapper,
                            ResponseDtoMapper<OrderResponseDto, Order> responseMapper) {
         this.orderService = orderService;
         this.productMapper = productMapper;
-        this.productService = productService;
-        this.autoOwnerService = autoOwnerService;
         this.requestMapper = requestMapper;
         this.responseMapper = responseMapper;
     }
@@ -50,8 +40,6 @@ public class OrderController {
     @ApiOperation(value = "create new Order")
     public OrderResponseDto create(@RequestBody OrderRequestDto requestDto) {
         Order order = orderService.save(requestMapper.toModel(requestDto));
-        AutoOwner autoOwner = autoOwnerService.getAutoOwnerByAutoId(requestDto.getAutoId());
-        autoOwnerService.save(autoOwnerService.addOrder(autoOwner.getId(), order));
         return responseMapper.toDto(order);
     }
 
