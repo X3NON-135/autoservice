9214b382b480e2f5b4e0f62d920afc9a7621d5ff
X3NON-135 <immort4ld3momo@gmail.com>
2022-12-18 19:02:33 +0000

    added new method to fetch data within one query

--- src/main/java/rest/autoservice/repository/OrderRepository.java
+++ src/main/java/rest/autoservice/repository/OrderRepository.java
@@ -1,9 +1,14 @@
 package rest.autoservice.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import rest.autoservice.model.Order;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    @Query(value = "SELECT * FROM orders o JOIN orders_products op ON o.id = op.order_id "
+            + " WHERE o.id = :id", nativeQuery = true)
+    Optional<Order> findByOrderId(Long id);
 }
