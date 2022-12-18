9333ad2e2da8e194d9ab72023b2a74d2d40de417
X3NON-135 <immort4ld3momo@gmail.com>
2022-12-18 19:02:03 +0000

    added new method to fetch data within one query

--- src/main/java/rest/autoservice/repository/AutoOwnerRepository.java
+++ src/main/java/rest/autoservice/repository/AutoOwnerRepository.java
@@ -1,12 +1,17 @@
 package rest.autoservice.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import rest.autoservice.model.AutoOwner;
+import rest.autoservice.model.Order;
 
 @Repository
 public interface AutoOwnerRepository extends JpaRepository<AutoOwner, Long> {
     @Query("SELECT ao FROM AutoOwner ao INNER JOIN ao.autos a where a.id = :id")
     AutoOwner getAutoOwnerByAutoId(Long id);
+
+    @Query("SELECT o FROM Order o INNER JOIN o.auto a WHERE a.owner.id = :id")
+    List<Order> getOrdersByAutoOwnerId(Long id);
 }
