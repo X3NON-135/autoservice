ef08581a385c76e80c58c162d1bf78b35cea068b
X3NON-135 <immort4ld3momo@gmail.com>
2023-01-10 20:42:15 +0000

    angular front was added

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,46 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {OrdersComponent} from "./orders/orders.component";
+import {RouterModule, Routes} from '@angular/router';
+import {OwnersComponent} from "./owners/owners.component";
+import {DutiesComponent} from "./duties/duties.component";
+import {ProductsComponent} from "./products/products.component";
+import {MastersComponent} from "./masters/masters.component";
+import {AutosComponent} from "./autos/autos.component";
 
-
+const routes: Routes = [{
+  path: 'auto_service',
+  children: [
+    {
+      path: 'orders',
+      component: OrdersComponent
+    },
+    {
+      path: 'owners',
+      component: OwnersComponent
+    },
+    {
+      path: 'duties',
+      component: DutiesComponent
+    },
+    {
+      path: 'products',
+      component: ProductsComponent
+    },
+    {
+      path: 'masters',
+      component: MastersComponent
+    },
+    {
+      path: 'autos',
+      component: AutosComponent
+    }
+  ]
+}
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,45 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AutosComponent} from './autos/autos.component';
+import {DutiesComponent} from './duties/duties.component';
+import {MastersComponent} from './masters/masters.component';
+import {OrdersComponent} from './orders/orders.component';
+import {OwnersComponent} from './owners/owners.component';
+import {ProductsComponent} from './products/products.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatDividerModule} from "@angular/material/divider";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AutosComponent,
+    DutiesComponent,
+    MastersComponent,
+    OrdersComponent,
+    OwnersComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatDividerModule
+  ],
+  exports: [
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+}
